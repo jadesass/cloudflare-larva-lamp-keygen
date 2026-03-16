ab11dbce1bc49e11fef4f8c0381e41eada038262
Jade Sass <pelumiezekiel.developer@gmail.com>
2026-03-16 19:58:23 +0100

    add entropy  from image

--- app.py
+++ app.py
@@ -7,3 +7,7 @@ from datetime import datetime
 class ImageEntropyGenerator:
     def __init__(self):
         self.entropy_pool = bytearray()
+    
+    def add_image_entropy(self, define_image_path):
+        with Image.open(define_image_path) as img:
+            img = img.convert('RGB')
